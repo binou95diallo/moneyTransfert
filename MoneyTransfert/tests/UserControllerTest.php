cc393df2a702a0c159a5a80c072897aff6bb1e6b
binetou diallo <perlbrillante@hotmail.fr>
2019-07-30 08:38:03 +0000

    test fonctionnel

--- MoneyTransfert/tests/UserControllerTest.php
+++ MoneyTransfert/tests/UserControllerTest.php
@@ -6,12 +6,45 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
 {
-    public function testSomething()
+    public function testRegister()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('POST', '/api/register',[],[],
+        ['CONTENT_TYPE'=>"application/json"],
+        '{
+            "username":"malick",
+            "password":"123",
+            "nomComplet":"malick",
+            "adresse":"mbour",
+            "telephone":"2214478920",
+            "email":"malick@malick.fr",
+            "profil":"admin",
+            "status":"debloquer"
 
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+        }'
+    
+    );
+    $reponse=$client->getResponse();
+        $this->assertSame(401,$reponse->getStatusCode());
+    }
+
+    public function testAjoutKo()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('POST', '/api/register',[],[],
+        ['CONTENT_TYPE'=>"application/json"],
+        '{
+            "username":"malick",
+            "password":"123",
+            "nomComplet":"malick",
+            "adresse":"mbour",
+            "telephone":"2214478920",
+            "email":"malick@malick.fr",
+            "profil":"admin",
+            "status":1
+        }');
+        $rep=$client->getResponse();
+        var_dump($rep);
+        $this->assertSame(401,$client->getResponse()->getStatusCode());
     }
 }
